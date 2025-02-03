6f5bcdf0da18167b6e67c579d729028f5853161e
Andrew Lipscomb <lipscomb.andrew@gmail.com>
2025-02-04 10:36:28 +1100

    - Adds f64 support via a feature flag on the crate - Deprecates the from_f32 method for PosRot as this is now misleading - Adds from_floats as a more generically named method - Updates documentation and tests

--- examples/flamegraph.rs
+++ examples/flamegraph.rs
@@ -1,36 +1,37 @@
 #![allow(clippy::incompatible_msrv)]
 
-use core::{f32::consts::PI, hint::black_box};
+use core::hint::black_box;
 
-use dubins_paths::DubinsPath;
+use dubins_paths::{DubinsPath, FloatType, PI};
 use rand::Rng;
 
 fn main() {
     let runs = 1_000_000;
+    let range: FloatType = 10000.0;
 
     let mut thread_rng = rand::thread_rng();
 
     for _ in 0..runs {
         let q0 = [
-            thread_rng.gen_range(-10000_f32..10000.),
-            thread_rng.gen_range(-10000_f32..10000.),
-            thread_rng.gen_range((-2. * PI)..(2. * PI)),
+            thread_rng.gen_range(-range..range),
+            thread_rng.gen_range(-range..range),
+            thread_rng.gen_range((-2. as FloatType * PI)..(2. as FloatType * PI)),
         ]
         .into();
         let q1 = [
-            thread_rng.gen_range(-10000_f32..10000.),
-            thread_rng.gen_range(-10000_f32..10000.),
-            thread_rng.gen_range((-2. * PI)..(2. * PI)),
+            thread_rng.gen_range(-range..range),
+            thread_rng.gen_range(-range..range),
+            thread_rng.gen_range((-2. as FloatType * PI)..(2. as FloatType * PI)),
         ]
         .into();
 
-        let rho = thread_rng.gen_range(600f32..3000.);
+        let rho = thread_rng.gen_range((600.0 as FloatType)..(3000. as FloatType));
 
         let Ok(path) = DubinsPath::shortest_from(black_box(q0), q1, rho) else {
             continue;
         };
 
-        let step_distance = thread_rng.gen_range(5f32..(rho / 100.));
+        let step_distance = thread_rng.gen_range((5.0 as FloatType)..(rho / (100.0 as FloatType)));
         let _ = path.sample_many(black_box(step_distance));
     }
 }
